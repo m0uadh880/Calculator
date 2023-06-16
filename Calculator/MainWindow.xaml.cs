276ce256459c6e83b44d44b4bd77ec937159f730
Mouadh Ounissi <ounissimouadh45@gmail.com>
2023-06-16 16:03:03 +0100

    -

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -15,14 +15,162 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+
+        private float firstNumber = 0 , secondNumber = 0 , result = 0;
+        private string operation;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void numberButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            string buttonContent = clickedButton.Content.ToString();
+
+
+            if(resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = buttonContent;
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{buttonContent}";
+            }
+        }
+
+        private void negativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() != "0")
+            {
+                resultLabel.Content = $"-{resultLabel.Content}";
+            }
+        }
+
+        private void percentageButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void divisionButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (resultLabel.Content.ToString() != "0" && int.TryParse(resultLabel.Content.ToString(), out int number))
+            {
+                Button clickedButton = (Button)sender;
+                string buttonContent = clickedButton.Content.ToString();
+
+                firstNumber = number;
+                operation = buttonContent;
+                resultLabel.Content = $"{resultLabel.Content} {buttonContent}";
+            }
+
+            else
+            {
+                resultLabel.Content = "0" ;
+            }
+
+        }
+
+        private void muliplicationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() != "0" && int.TryParse(resultLabel.Content.ToString(), out int number))
+            {
+                Button clickedButton = (Button)sender;
+                string buttonContent = clickedButton.Content.ToString();
+
+                firstNumber = number;
+                operation = buttonContent;
+                resultLabel.Content = $"{resultLabel.Content} {buttonContent}";
+            }
+
+            else
+            {
+                resultLabel.Content = "0";
+            }
+        }
+
+
+        private void subtractionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() != "0" && int.TryParse(resultLabel.Content.ToString(), out int number))
+            {
+                Button clickedButton = (Button)sender;
+                string buttonContent = clickedButton.Content.ToString();
+
+                firstNumber = number;
+                operation = buttonContent;
+                resultLabel.Content = $"{resultLabel.Content} {buttonContent}";
+            }
+
+            else
+            {
+                resultLabel.Content = "0";
+            }
+        }
+
+
+        private void additionButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() != "0" && int.TryParse(resultLabel.Content.ToString(), out int number))
+            {
+                Button clickedButton = (Button)sender;
+                string buttonContent = clickedButton.Content.ToString();
+
+                firstNumber = number;
+                operation = buttonContent;
+                resultLabel.Content = $"{resultLabel.Content} {buttonContent}";
+            }
+
+            else
+            {
+                resultLabel.Content = "0";
+            }
+        }
+
+        private void acButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void equalButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() != "0" && !int.TryParse(resultLabel.Content.ToString(), out int number))
+            {
+                secondNumber = int.Parse(resultLabel.Content.ToString().Substring(resultLabel.Content.ToString().Length - 1));
+
+                if(operation == "*")
+                {
+                    result = firstNumber * secondNumber;
+                }
+                else if(operation == "/")
+                {
+                    result = firstNumber / secondNumber   ;
+                }
+                else if (operation == "+")
+                {
+                    result = firstNumber + secondNumber;
+                }
+                else if (operation == "-")
+                {
+                    result = firstNumber - secondNumber;
+                }
+
+                resultLabel.Content = $"{result}";
+            }
+
+            else
+            {
+            }
         }
     }
 }
